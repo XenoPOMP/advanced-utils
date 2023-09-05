7522f30d420a4df55c573a3f82acae87cd96d4cd
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-09-05 21:32:15 +0300

    change exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { pullPattern } from './utils/pullPattern';
 
 export { pullPattern };
+
+export * from './type-checks/index';
+export * from './math-utils/index';
+export * from './string/index';
