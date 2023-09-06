9d7e3995f2bb6c800e889dba033c9c6ae133bb87
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-09-06 16:14:26 +0300

    change build config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { pullPattern } from './utils/pullPattern';
 
 export { pullPattern };
+
+export * from './math-utils/index';
+export * from './string/index';
+export * from './type-checks/index';
